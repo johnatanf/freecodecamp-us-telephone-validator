c49d270b42d246d56efa1e4e4a8bde90af3d3bd4
johnatanf <johnatan.f100@gmail.com>
2024-06-12 09:48:48 +0700

    create basic javascript functionality

---
+++ script.js
@@ -0,0 +1,49 @@
+initialiseApp()
+
+function validateInput(str) {
+    if(str === "") {
+        alert("Please provide a phone number")
+        return false
+    } else {
+        return true
+    }
+}
+
+function clearInput() {
+    const inputField = document.getElementById('user-input')
+    const resultDiv = document.getElementById('results-div')
+    
+    inputField.value = ""
+    resultDiv.innerHTML = ""
+}
+
+function validateUSNumber(str) {
+    return true
+}
+
+function clickCheckButton() {
+    const inputField = document.getElementById('user-input')
+    const resultsDiv = document.getElementById('results-div')
+
+    const inputValue = inputField.value
+
+    if(validateInput(inputValue) === true) {
+        if(validateUSNumber(inputValue) === true) {
+            resultsDiv.innerHTML = `Valid US number: ${inputValue}`
+        } else {
+            resultsDiv.innerHTML = `Invalid US number: ${inputValue}`
+        }
+    }
+}
+
+function clickClearButton() {
+    clearInput()
+}
+
+function initialiseApp() {
+    const checkButton = document.getElementById('check-btn')
+    const clearButton = document.getElementById('clear-btn')
+
+    checkButton.addEventListener('click', clickCheckButton)
+    clearButton.addEventListener('click', clickClearButton)
+}
